318f9745ebbab021e4d21eb2d08408b07ee0bf46
v4g0 <arturo.avbraca@gmail.com>
2019-04-05 14:18:45 -0600

    redux-dev-tools added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,21 @@
-import { connectRouter, routerMiddleware } from 'connected-react-router'
+import { routerMiddleware } from 'connected-react-router'
 import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/RootReducer';
 import { history } from '../helpers/history';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const loggerMiddleware = createLogger()
 
-// export const store = createStore(
-//     // connectRouter(history)(rootReducer), // new root reducer with router state
-//     // connectRouter(rootReducer), // new root reducer with router state
-//     rootReducer,
-//     compose(
-//       applyMiddleware(
-//         thunkMiddleware, // lets us dispatch() functions
-//         loggerMiddleware, // neat middleware that logs actions
-//         // routerMiddleware(history), // for dispatching history actions
-//       ),
-//     ),
-// );
-
+// const store = createStore(reducer, composeWithDevTools(
+//   applyMiddleware(...middleware),
+//   // other store enhancers if any
+// ));
 
 export const store = createStore(
   rootReducer, // new root reducer with router state
-  compose(
+  composeWithDevTools(
       applyMiddleware(
           thunkMiddleware, // lets us dispatch() functions
           // loggerMiddleware, // neat middleware that logs actions
